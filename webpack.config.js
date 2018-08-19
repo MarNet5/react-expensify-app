b6932e1e02ab412189c232ed8d7d816928f91bf5
Marina Netrebchuk <marinanetrebchuk@MacBook-Air-Marina.local>
2018-08-19 16:24:13 +0300

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,50 @@
 //entry -> output
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    //loader
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,  //regexp finding all files end with .js
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,  //regexp finding all files end with .css and .scss
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true //tells devserver we're gona be handling routing via our client side code
-    }
-};
-
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        //loader
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,  //regexp finding all files end with .js
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,  //regexp finding all files end with .css and .scss
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true //tells devserver we're gona be handling routing via our client side code
+        }
+    };
+    
+}
