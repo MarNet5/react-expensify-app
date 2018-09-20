f1c1ee15efd5f8cacbc920202798647bdd45865f
Marina Netrebchuk <marinanetrebchuk@MacBook-Air-Marina.local>
2018-09-20 15:57:21 +0300

    Setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -30,6 +30,16 @@ export const removeExpense = ({ id } = {}) => ({
     type: 'REMOVE_EXPENSE',
     id
 });
+
+//START_REMOVE_EXPENSE
+export const startRemoveExpense = ({ id } = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({ id }));
+        });
+    };
+};
+
 //EDIT_EXPENSE
 
 export const editExpense = (id, updates) => ({
@@ -60,3 +70,5 @@ export const startSetExpenses = () => {
           });
     };
 };
+
+
